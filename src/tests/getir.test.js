accd88f81852f20b2df0f720973f6451102c540b
imSidJ <sid786india@gmail.com>
2021-07-23 17:49:05 +0530

    Updated test cases

--- src/tests/getir.test.js
+++ src/tests/getir.test.js
@@ -4,7 +4,109 @@ const { getirService } = require('../services');
 
 const app = require('../app');
 
-describe('POST /getir', () => {
+describe('Check request', () => {
+  it('should return 400 when startDate is missing', (done) => {
+    request(app)
+      .post('/getir')
+      .set('content-type', 'application/json')
+      .send({
+        endDate: '2018-02-02',
+        minCount: 2500,
+        maxCount: 3000,
+      })
+      .expect(400, done);
+  });
+
+  it('should return 400 when endDate is missing', (done) => {
+    request(app)
+      .post('/getir')
+      .set('content-type', 'application/json')
+      .send({
+        startDate: '2018-02-02',
+        minCount: 2500,
+        maxCount: 3000,
+      })
+      .expect(400, done);
+  });
+
+  it('should return 400 when minCount is missing', (done) => {
+    request(app)
+      .post('/getir')
+      .set('content-type', 'application/json')
+      .send({
+        startDate: '2018-02-01',
+        endDate: '2018-02-02',
+        minCount: 2500,
+      })
+      .expect(400, done);
+  });
+
+  it('should return 400 when maxCount is missing', (done) => {
+    request(app)
+      .post('/getir')
+      .set('content-type', 'application/json')
+      .send({
+        startDate: '2018-02-02',
+        endDate: '2018-02-02',
+        maxCount: 3000,
+      })
+      .expect(400, done);
+  });
+
+  it('should return 400 when startDate has invalid format', (done) => {
+    request(app)
+      .post('/getir')
+      .set('content-type', 'application/json')
+      .send({
+        startDate: '2016-01-2',
+        endDate: '2018-02-02',
+        minCount: 2500,
+        maxCount: 3000,
+      })
+      .expect(400, done);
+  });
+
+  it('should return 400 when endDate has invalid format', (done) => {
+    request(app)
+      .post('/getir')
+      .set('content-type', 'application/json')
+      .send({
+        startDate: '2016-01-02',
+        endDate: '2018-02-1',
+        minCount: 2500,
+        maxCount: 3000,
+      })
+      .expect(400, done);
+  });
+
+  it('should return 400 when minCount has invalid format', (done) => {
+    request(app)
+      .post('/getir')
+      .set('content-type', 'application/json')
+      .send({
+        startDate: '2016-01-02',
+        endDate: '2018-02-02',
+        minCount: '2500',
+        maxCount: 3000,
+      })
+      .expect(400, done);
+  });
+
+  it('should return 400 when maxCount has invalid format', (done) => {
+    request(app)
+      .post('/getir')
+      .set('content-type', 'application/json')
+      .send({
+        startDate: '2016-01-02',
+        endDate: '2018-02-02',
+        minCount: 2500,
+        maxCount: '3000',
+      })
+      .expect(400, done);
+  });
+});
+
+describe('Check Response', () => {
   const stub = sinon.stub(getirService, 'getData');
 
   it('should return success if no matching records found', async () => {
