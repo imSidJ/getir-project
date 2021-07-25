cee412c7a5b7a905b6b9bc5e97206636ae4b962a
imSidJ <sid786india@gmail.com>
2021-07-25 13:15:16 +0530

    add helmet

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const swaggerUi = require('swagger-ui-express');
 const OpenApiValidator = require('express-openapi-validator');
 const YAML = require('yamljs');
 const path = require('path');
-var cors = require('cors');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const routes = require('./routes');
 const apiKeyCheck = require('./middlewares/apiKeyCheck');
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 app.use(limiter);
 
 const swaggerDocument = YAML.load(path.join(__dirname, './docs/api.yml'));
